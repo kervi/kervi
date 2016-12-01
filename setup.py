b87bfb1af731928377a00cd2f4c84ab38a6d5302
Tim Wentzlau <tim.wentzlau@auxilior.com>
2016-12-01 22:03:25 +0000

    restructured project

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'kervi',
+  packages = ['kervi'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A python framework for creating robotic and automation applications.',
+  author = 'Tim Wentzlau',
+  author_email = 'tim.wentzlau@gmail.com',
+  url = 'https://github.com/wentzlau/kervi', # use the URL to the github repo
+  download_url = 'https://github.com/wentzlau/kervi/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
