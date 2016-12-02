0d514775979972c42b74e3807e160af5f087496e
Tim Wentzlau <tim.wentzlau@auxilior.com>
2016-12-02 14:44:25 +0000

    Updated npm package defenitions updated setup.py to include html files

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 from distutils.core import setup
+from distutils import dir_util
+from distutils import sysconfig
+import os
+
 setup(
-  name = 'kervi',
-  packages = ['kervi'], # this must be the same as the name above
-  version = '0.1',
-  description = 'A python framework for creating robotic and automation applications.',
-  author = 'Tim Wentzlau',
-  author_email = 'tim.wentzlau@gmail.com',
-  url = 'https://github.com/wentzlau/kervi', # use the URL to the github repo
-  download_url = 'https://github.com/wentzlau/kervi/tarball/0.1', # I'll explain this in a second
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
-  classifiers = [],
+    name='kervi',
+    packages=['.', 'utility', 'coreSensors', 'coreSensors/cpuSensors','kervi-ui/dist'],
+    version='0.1',
+    description='A python framework for creating robotic and automation applications on raspberypi (and other platforms).',
+    author='Tim Wentzlau',
+    author_email='tim.wentzlau@gmail.com',
+    url='https://github.com/wentzlau/kervi',
+    download_url='https://github.com/wentzlau/kervi/archive/v1.0-alpha.tar.gz',
+    keywords=['raspberry pi', 'robotic', 'automation'],
+    classifiers=[],
+    include_package_data=True,
+    package_data={
+        '': ['*.html', '*.js', '*.css', '*.map', '*.ico', '*.eot', '*.svg', '*.woff', '*.woff2', '*.ttf'],
+    },
 )
+
