d1cbf72c2a84416354abbcdb8a3a2bfadc361117
Tim Wentzlau <tim.wentzlau@auxilior.com>
2016-12-04 22:56:07 +0000

    Initial cli interface coding

---
+++ kervi/kervi.py
@@ -0,0 +1,10 @@
+import click
+
+@click.group()
+@click.option('--debug/--no-debug', default=False)
+def cli(debug):
+    click.echo('Debug mode is %s' % ('on' if debug else 'off'))
+
+@cli.command()
+def init():
+    click.echo('Synching')
