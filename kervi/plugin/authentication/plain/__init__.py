7ced79786934f83b045a2ea9c5b971036e06498b
Tim Wentzlau <tim@wentzlau.dk>
2018-09-18 22:05:48 +0100

    Move to plugin section

---
+++ kervi/plugin/authentication/plain/__init__.py
@@ -0,0 +1,31 @@
+from kervi.plugin.authentication.authentication_handler import AuthenticationHandler
+
+
+class PlainAuthenticationHandler(AuthenticationHandler):
+    def __init__(self, configuration):
+        AuthenticationHandler.__init__(self, "plain", configuration)
+
+    def allow_anonymous(self):
+        if "anonymous" in self.configuration.authentication.users.keys:
+            return self.configuration.authentication.users.anonymous.enabled
+        return False
+    
+    def authorize(self, user_name, password):
+        print("au", user_name)
+        users = self.configuration.authentication.users
+        user = users.get(user_name, None)
+        if user:
+            if user_name == "anonymous":
+                if self.allow_anonymous():
+                    return user
+            else:
+                user_password = user.get("password", None)
+                enabled = user.get("enabled", False)
+                if user_password and enabled and user_password == password:
+                    return user
+                elif not user_password and not password:
+                    return user
+        return None
+
+def init_plugin(config):
+    return PlainAuthenticationHandler(config)
