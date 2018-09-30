3cbb3e13e004d5c99773a1ef9c2506322afc8522
Tim Wentzlau <tim@wentzlau.dk>
2018-09-30 23:16:27 +0100

    Update start and stop handling

--- kervi/application/__init__.py
+++ kervi/application/__init__.py
@@ -28,19 +28,25 @@ try:
 except ImportError:
     import _thread as thread
 
-from kervi.application.kervi_module import KerviModule
-import kervi.core.utility.process as process
-import kervi.spine as spine
+#from kervi.application.kervi_module import KerviModule
+#import kervi.core.utility.process as process
+#import kervi.spine as spine
 #from kervi.utility.process_spine import _ProcessSpine
 #import kervi.kervi_logging as logging
-import kervi.ui.webserver as webserver
 import kervi.utility.nethelper as nethelper
-import kervi.utility.encryption as encryption
-import kervi.utility.application_helpers as app_helpers
+#import kervi.utility.encryption as encryption
 from kervi.controllers.controller import Controller
 from kervi.actions import action
 from kervi.application.default_config import get_default_config
 
+import signal
+
+_app_running = True
+
+def handler_stop_signals(signum, frame):
+    global _app_running
+    _app_running = False
+    print("signal:", signum)
 
 def _pretty_print(d, indent=0):
     if isinstance(d, dict):
@@ -70,18 +76,23 @@ class _AppActions(Controller):
 
     @action
     def shutdown(self):
-        print("shutwown app")
-        self._app.stop()
+        print("shutdown app device")
+        self._app._shutdown_action()
 
     @action
     def reboot(self):
         print("reboot app device")
-        self._app.stop()
+        self._app._reboot_action()
 
     @action
     def restart(self):
         print("restart app")
-        self._app.stop()
+        self._app._restart_action()
+
+    @action
+    def stop(self):
+        print("stop app")
+        self._app._stop_action()
 
 class Application(object):
     """ Kervi application class that starts a kervi application and loads all needed modules.
@@ -91,14 +102,18 @@ class Application(object):
     def __init__(self, user_config = None):
         """ Settings is a dictionary with the following content
         """
+        import sys
+        if sys.version_info[0] < 3:
+            print("Kervi requires Python 3")
+            exit()
+        
         print("Starting kervi application")
         import inspect
         import getopt
-        import sys
-
+        
         config_files = []
 
-        opts, args = getopt.getopt(sys.argv[1:], "c", ["config_file=", "service_install", "service_uninstall", "service_start", "service_stop", "detect_devices"])
+        opts, args = getopt.getopt(sys.argv[1:], "c", ["config_file=", "as-service", "install-service", "uninstall-service", "start-service", "stop-service", "restart-service", "status-service", "detect-devices"])
         for opt, arg in opts:
             if opt in ("-c", "--config_file"):
                 if os.path.isfile(arg):
@@ -129,24 +144,30 @@ class Application(object):
 
         service_commands = []
         detect_devices = None
+        self._as_service = False
         for opt, arg in opts:
-            if opt in ("--detect_devices"):
+            if opt in ("--as-service"):
+                self._as_service = True
+
+            if opt in ("--detect-devices"):
                 detect_devices = True
 
-            if opt in ("--service_install"):
+            if opt in ("--install-service"):
                 service_commands += ["install"]
 
-            if opt in ("--service_uninstall"):
+            if opt in ("--uninstall-service"):
                 service_commands += ["uninstall"]
 
-            if opt in ("--service_start"):
+            if opt in ("--start-service"):
                 service_commands += ["start"]
 
-            if opt in ("--service_stop"):
-                service_commands = "stop"
+            if opt in ("--stop-service"):
+                service_commands = ["stop"]
             
-            if opt in ("--service_restart"):
-                service_commands = "restart"
+            if opt in ("--restart-service"):
+                service_commands = ["restart"]
+            if opt in ("--service-status"):
+                service_commands = ["status"]
 
         if service_commands:
             import kervi.hal as hal
@@ -174,6 +195,7 @@ class Application(object):
         #    self.settings = app_helpers._deep_update(self.settings, settings)
         #self._validateSettings()
         self.started = False
+        import kervi.spine as spine
         from kervi.zmq_spine import _ZMQSpine
         self.spine = _ZMQSpine()
         self.spine._init_spine("kervi-main", self.config.network.ipc_root_port, None, self.config.network.ipc_root_address)
@@ -293,8 +315,15 @@ class Application(object):
             import dashboards
         except ImportError:
             pass
+        
+        import kervi.core.utility.process as process
+        import kervi.utility.application_helpers as app_helpers
 
-        #self.spine.run()
+
+        if self._as_service:
+            signal.signal(signal.SIGINT, handler_stop_signals)
+            signal.signal(signal.SIGTERM, handler_stop_signals)
+
         self.spine.send_command("startThreads", local_only=True)
         time.sleep(.5)
         module_port = self.config.network.ipc_root_port
@@ -356,6 +385,7 @@ class Application(object):
         else:
             print("Your Kervi application is ready at http://" + self.config.network.ip + ":" + str(self.config.network.http_port))
         print("Press ctrl + c to stop your application")
+        import kervi.ui.webserver as webserver
         webserver.start(
             self.config.network.ip,
             self.config.network.http_port,
@@ -385,7 +415,7 @@ class Application(object):
         try:
             char_list = []
             thread.start_new_thread(self._input_thread, (char_list,))
-            while not char_list:
+            while not char_list and _app_running:
                 self._check_kervi_modules()
                 time.sleep(1)
 
@@ -400,10 +430,13 @@ class Application(object):
         if not self.started:
             self._start()
 
-    def stop(self, force_exit=True):
+    def stop(self, force_exit=True, restart=False):
         self.spine.send_command("kervi_action_app_exit")
+        import kervi.ui.webserver as webserver
+        
         webserver.stop()
         print("stopping processes")
+        import kervi.core.utility.process as process
         process._stop_processes("app-" + self.config.application.id)
         self.spine.trigger_event("processTerminating", None, local_only=True)
         time.sleep(1)
@@ -415,3 +448,17 @@ class Application(object):
         if force_exit:
             import os
             os._exit(0)
+
+    def _stop_action(self):
+        self.stop()
+
+    def _restart_action(self):
+        self.stop(restart=True)
+
+    def _reboot_action(self):
+        import kervi.hal as hal
+        hal.device_reboot()
+
+    def _shutdown_action(self):
+        import kervi.hal as hal
+        hal.device_shutdown()
--- kervi/module/__init__.py
+++ kervi/module/__init__.py
@@ -42,6 +42,34 @@ from kervi.actions import action, Actions
 from kervi.controllers.controller import Controller
 from kervi.module.default_config import get_default_config
 
+_app_running = True
+
+def handler_stop_signals(signum, frame):
+    global _app_running
+    _app_running = False
+    print("signal:", signum)
+
+
+def _pretty_print(d, indent=0):
+    if isinstance(d, dict):
+        for key in d.keys():
+            value = d[key]
+            
+            if isinstance(value, dict) or isinstance(value, list):
+                print( '  ' * indent + str(key))
+                _pretty_print(value, indent+1)
+            else:
+                print('  ' * (indent+1) + key + ":" +  str(value))
+    elif isinstance(d, list):
+        for item in d:
+            if isinstance(item, dict) or isinstance(item, list):
+                _pretty_print(item, indent+1)
+            else:
+                print('  ' * (indent+1) + str(item))
+    else:
+        pass
+
+
 class _ModuleActions(Controller):
     def __init__(self, module):
         super().__init__(module.config.module.id, "Module")
@@ -50,23 +78,23 @@ class _ModuleActions(Controller):
 
     @action
     def shutdown(self):
-        print("shutwown module device")
-        self._module.stop()
+        print("shutdown module device")
+        self._module._shutdown_action()
 
     @action
     def reboot(self):
         print("reboot module device")
-        self._module.stop()
+        self._module._reboot_action()
 
     @action
     def stop(self):
         print("stop module")
-        self._module.stop()
+        self._module._stop_action()
 
     @action
     def restart(self):
         print("restart module")
-        self._module.stop()
+        self._module._restart_action()
 
 
 
@@ -79,13 +107,25 @@ class Module(object):
 
         from kervi.config import load
         import inspect
-        #import sys
+        import sys
         import os
 
-        script_name = os.path.basename(os.path.abspath(inspect.stack()[1][1]))
-        script_name, script_ext = os.path.splitext(script_name)
+        script_path = os.path.basename(os.path.abspath(inspect.stack()[1][1]))
+        script_name, script_ext = os.path.splitext(script_path)
 
         config_files = []
+        
+        import getopt
+        opts, args = getopt.getopt(sys.argv[1:], "c", ["config_file=", "as-service", "install-service", "uninstall-service", "start-service", "stop-service", "restart-service", "status-service", "detect-devices"])
+        for opt, arg in opts:
+            if opt in ("-c", "--config_file"):
+                if os.path.isfile(arg):
+                    config_files += [arg]
+                else:
+                    print("Specified config file not found:", arg)
+        
+        
+        
         config_files += [script_name +".config.json"]
         config_files += ["kervi.config.json"]
 
@@ -96,14 +136,62 @@ class Module(object):
                 break
         if selected_config_file:
             print("using config file:", selected_config_file)
-        else:
-            print("no config file found, revert to defaults")
+        #else:
+        #    print("no config file found, revert to defaults")
 
         self.config = load(
             config_file=selected_config_file,
             config_user=user_config,
             config_base= get_default_config()
         )
+
+        service_commands = []
+        detect_devices = None
+        self._as_service = False
+        for opt, arg in opts:
+            if opt in ("--as-service"):
+                self._as_service = True
+
+            if opt in ("--detect-devices"):
+                detect_devices = True
+
+            if opt in ("--install-service"):
+                service_commands += ["install"]
+
+            if opt in ("--uninstall-service"):
+                service_commands += ["uninstall"]
+
+            if opt in ("--start-service"):
+                service_commands += ["start"]
+
+            if opt in ("--stop-service"):
+                service_commands = ["stop"]
+            
+            if opt in ("--restart-service"):
+                service_commands = ["restart"]
+            if opt in ("--service-status"):
+                service_commands = ["status"]
+
+        if service_commands:
+            import kervi.hal as hal
+            hal_driver = hal._load(self.config.platform.driver)
+            if hal_driver:
+                hal.service_commands(
+                    service_commands,
+                    self.config.application.name,
+                    self.config.application.id,
+                    script_path
+                )
+            exit()
+
+        if detect_devices:
+            import kervi.hal as hal
+            hal_driver = hal._load(self.config.platform.driver)
+            if hal_driver:
+                devices = hal.detect_devices()
+                print("devices:")
+                _pretty_print(devices)
+            exit()
 
         print("Starting kervi module, please wait")
         self.started = False
@@ -175,6 +263,11 @@ class Module(object):
 
     def _start(self):
         self.started = True
+
+        if self._as_service:
+            import signal
+            signal.signal(signal.SIGINT, handler_stop_signals)
+            signal.signal(signal.SIGTERM, handler_stop_signals)
 
         try:
             import dashboards
@@ -249,7 +342,7 @@ class Module(object):
         try:
             char_list = []
             thread.start_new_thread(self._input_thread, (char_list,))
-            while not char_list:
+            while not char_list and _app_running:
                 self.spine.trigger_event("modulePing", self.config.module.id)
                 time.sleep(1)
 
@@ -264,17 +357,34 @@ class Module(object):
         if not self.started:
             self._start()
 
-    def stop(self):
+    def stop(self, force_exit=True, restart=False):
+        self.spine.send_command("kervi_action_module_exit")
         self.spine.trigger_event(
             "moduleStopped",
             self.config.module.id
         )
         print("stopping processes")
         process._stop_processes("module-" + self.config.module.id)
+        self.spine.trigger_event("processTerminating", None, local_only=True)
         time.sleep(1)
         spine.Spine().stop()
         #for thread in threading.enumerate():
         #    print("running thread",thread.name)
         print("module stopped")
-        import os
-        os._exit(0)
+        if force_exit:
+            import os
+            os._exit(0)
+
+    def _stop_action(self):
+        self.stop()
+
+    def _restart_action(self):
+        self.stop(restart=True)
+
+    def _reboot_action(self):
+        import kervi.hal as hal
+        hal.device_reboot()
+
+    def _shutdown_action(self):
+        import kervi.hal as hal
+        hal.device_shutdown()
