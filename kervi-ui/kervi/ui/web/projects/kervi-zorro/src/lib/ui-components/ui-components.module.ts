ede417dd2c9e3ed10e4c8a13521580271f9778ae
Tim Wentzlau <tim@wentzlau.dk>
2020-08-02 16:26:02 +0100

    nipplejs support in controller pads

--- kervi-ui/kervi/ui/web/projects/kervi-zorro/src/lib/ui-components/ui-components.module.ts
+++ kervi-ui/kervi/ui/web/projects/kervi-zorro/src/lib/ui-components/ui-components.module.ts
@@ -1,25 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common'
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
-import { NGXKerviPipesModule  } from 'ngx-kervi'
+import { NGXKerviPipesModule  } from 'ngx-kervi';
 import { SparklineComponent } from './sparkline/sparkline.component';
 import { SliderComponent } from './slider/slider.component';
 import { GaugeComponent } from './gauge/gauge.component';
-import { KerviChartComponent } from './chart/chart.component'
-//import { CommonModule } from '@angular/common';
+import { KerviChartComponent } from './chart/chart.component';
 import { SwitchButtonComponent} from './switch-button/switch-button.component';
 import { ButtonComponent} from './button/button.component';
-//import { CamViewerComponent } from './cam-viewer/cam-viewer.component';
 import { MPEGViewerComponent } from './mpeg-viewer/mpeg-viewer.component';
-//import { ImageViewerComponent } from './image-viewer/image-viewer.component'; 
-//import { ActionComponent } from './action/action.component';
 import { DateTimeComponent } from './datetimepicker/datetimepicker.component';
 import { IconsComponent } from './icons/icons.component';
 import { NgxGaugeModule } from 'ngx-gauge';
 import { ColorComponent } from './color/color.component';
-import { FormsModule }    from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
-import { NgApexchartsModule } from 'ng-apexcharts'
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 @NgModule({
   declarations: [
@@ -30,10 +26,7 @@ import { NgApexchartsModule } from 'ng-apexcharts'
     KerviChartComponent,
     SwitchButtonComponent,
     ButtonComponent,
-    //CamViewerComponent,
     MPEGViewerComponent,
-    //ImageViewerComponent,
-    //ActionComponent,
     DateTimeComponent,
     ColorComponent
   ],
@@ -46,8 +39,6 @@ import { NgApexchartsModule } from 'ng-apexcharts'
       KerviChartComponent,
       SwitchButtonComponent,
       ButtonComponent,
-      //CamViewerComponent,
-      //ActionComponent,
       DateTimeComponent,
       ColorComponent
   ],
@@ -59,11 +50,10 @@ import { NgApexchartsModule } from 'ng-apexcharts'
     FormsModule,
     NgZorroAntdModule,
     NgApexchartsModule
-    //KerviPipesModule
   ],
   providers: [],
   bootstrap: []
 })
 export class UIComponentsModule {
-  constructor (){}
+  constructor () {}
 }
