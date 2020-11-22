287ccdbdb3553d2aa1378a630e3dc2115a000dce
Tim Wentzlau <tim@wentzlau.dk>
2020-11-22 21:17:17 +0000

    Add dev install script for linux

---
+++ install_dev.sh
@@ -0,0 +1,13 @@
+
+pip3 install zmq
+pip3 install pillow
+pip3 install autobahn
+pip3 install -e ./kervi-devices
+pip3 install -e ./kervi-hal-generic
+pip3 install -e ./kervi-hal-rpi
+pip3 install -e ./kervi-hal-win
+pip3 install -e ./kervi-hal-linux
+pip3 install -e ./kervi-cli
+pip3 install -e ./kervi-core
+pip3 install --no-dependencies -e ./kervi
+
