817c318e2d2a33246521f4d4387a69abcbf4318c
Tim Wentzlau <tim@wentzlau.dk>
2021-04-04 21:46:45 +0100

    update versions and install order

--- install_dev.bat
+++ install_dev.bat
@@ -1,15 +1,15 @@
 
-pip install zmq
+pip install pyzmq==22.0.3
 pip install pillow
 pip install autobahn
 
+pip install --no-dependencies -e ./kervi
+pip install -e ./kervi-ui
 pip install -e ./kervi-devices
 pip install -e ./kervi-hal-generic
 pip install -e ./kervi-hal-rpi
 pip install -e ./kervi-hal-win
 pip install -e ./kervi-cli
 pip install -e ./kervi-core
-pip install -e ./kervi-ui
-pip install --no-dependencies -e ./kervi
 
 pause
--- install_dev.sh
+++ install_dev.sh
@@ -1,13 +1,14 @@
 
-pip3 install zmq
+pip3 install pyzmq==22.0.3
 pip3 install pillow
 pip3 install autobahn
+
+pip3 install --no-dependencies -e ./kervi
+pip3 install -e ./kervi-core
 pip3 install -e ./kervi-devices
 pip3 install -e ./kervi-hal-generic
 pip3 install -e ./kervi-hal-rpi
 pip3 install -e ./kervi-hal-win
 pip3 install -e ./kervi-hal-linux
 pip3 install -e ./kervi-cli
-pip3 install -e ./kervi-core
-pip3 install --no-dependencies -e ./kervi
 
